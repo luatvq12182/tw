47030e248bd6ee8f818153136b5301b4deafcf23
Leo <leo.brightsoft@gmail.com>
2023-11-06 11:33:01 +0700

    Vite config

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
+    plugins: [react()],
     server: {
         watch: {
             usePolling: true,
